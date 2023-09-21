4c0363b957026090fb003091578fb499205ae102
papabeanstalk <7cameronlee@gmail.com>
2023-09-21 01:25:57 -0700

    Started Elements list

--- scripts/ptable.js
+++ scripts/ptable.js
@@ -1,1 +1,20 @@
-document.write('<p>Testing my Git knowledge!</p>');
+function Element(sym, fullName, molMass){
+    this.symbol = sym;
+    this.name = fullName;
+    this.molarMass = molMass;
+};
+
+Element.prototype.getSymbol = function(){
+    return this.symbol;
+}
+
+Element.prototype.getName = function(){
+    return this.name;
+}
+Element.prototype.getMolMass = function(){
+    return this.molarMass;
+}
+
+const Hydrogen = new Element("H", "Hydrogen", 1.008);
+
+document.getElementById("H").innerHTML = Hydrogen.molarMass;
